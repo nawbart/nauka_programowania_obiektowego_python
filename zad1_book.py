2e258dd308c5638fb22839b173e227bfb829404f
bartek <nawrockibartoszwro@gmail.com>
2022-09-18 18:38:17 +0200

    stworenie obiektów, dodanie ich do listy oraz estetyczne wyświetlenie w konsoli

--- zad1_book.py
+++ zad1_book.py
@@ -1,11 +1,12 @@
 class book:
+
     title = ''
     author = ''
     yrOfPublishment = None
     nbrOfPages = None
     owner = ''
 
-    def __int__(self, title, author, yrOfPublishment, nbrOfPages, owner):
+    def __init__(self, title, author, yrOfPublishment, nbrOfPages, owner):
         self.title = title
         self.author = author
         self.yrOfPublishment = yrOfPublishment
@@ -14,3 +15,19 @@ class book:
 
     def newOwner(self, newOwner):
         self.owner = newOwner
+
+
+HP = book('Harry Potter', 'J.K Rowling', 1998, 499, 'Bartek')
+LoR = book('Lords of Rings', 'J.R.R Tolkien', 1995, 350, 'Michał')
+GM = book('Green Mile', 'S.King', 1996, 730, 'Kamila')
+
+list_of_books = []
+
+
+
+list_of_books.append(HP)
+list_of_books.append(LoR)
+list_of_books.append(GM)
+
+for i in list_of_books:
+    print('*********','\nTitle: ',i.title, '\nAuthor: ', i.author,'\nYear of publishment: ', i.yrOfPublishment, '\nNumber of pages:', i.nbrOfPages,'\nOwner: ', i.owner)
