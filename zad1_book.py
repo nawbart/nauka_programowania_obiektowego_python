a5e03ac01a4eab7e8b360fedba85bd3b7d22c633
bartek <nawrockibartoszwro@gmail.com>
2022-09-18 18:08:13 +0200

    dodanie konstruktora do klasy.

--- zad1_book.py
+++ zad1_book.py
@@ -5,5 +5,12 @@ class book:
     nbrOfPages = None
     owner = ''
 
+    def __int__(self, title, author, yrOfPublishment, nbrOfPages, owner):
+        self.title = title
+        self.author = author
+        self.yrOfPublishment = yrOfPublishment
+        self.nbrOfPages = nbrOfPages
+        self.owner = owner
+
     def newOwner(self, newOwner):
         self.owner = newOwner
