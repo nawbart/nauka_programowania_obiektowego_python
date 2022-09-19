fd3dcdf94b2c0f1432bac7029cc3e985ba1a5f6b
Bartosz Nawrocki <nawrockibartoszwro@gmail.com>
2022-09-19 17:30:58 +0200

    zmiany nazw zmiennych i dodanie type hint'ów

--- zad1_book.py
+++ zad1_book.py
@@ -1,27 +1,25 @@
-class book:
+class Book:
 
-    def __init__(self, title, author, yrOfPublishment, nbrOfPages, owner):
+    def __init__(self, title: str, author: str, yrOfPublishment: int, nbrOfPages: int, owner_name: str ):
         self.title = title
         self.author = author
         self.yrOfPublishment = yrOfPublishment
         self.nbrOfPages = nbrOfPages
-        self.owner = owner
+        self.owner_name = owner_name
 
-    def newOwner(self, newOwner):
-        self.owner = newOwner
+    def set_owner_name(self, owner_name):
+        self.owner_name = owner_name
 
 
-HP = book('Harry Potter', 'J.K Rowling', 1998, 499, 'Bartek')
-LoR = book('Lords of Rings', 'J.R.R Tolkien', 1995, 350, 'Przemysław')
-GM = book('Green Mile', 'S.King', 1996, 730, 'Kamila')
+HP = Book('Harry Potter', 'J.K Rowling', 1998, 499, 'Bartek')
+LoR = Book('Lords of Rings', 'J.R.R Tolkien', 1995, 350, 'Przemysław')
+GM = Book('Green Mile', 'S.King', 1996, 730, 'Kamila')
 
 list_of_books = []
-
-
 
 list_of_books.append(HP)
 list_of_books.append(LoR)
 list_of_books.append(GM)
 
 for i in list_of_books:
-    print('*********','\nTitle: ',i.title, '\nAuthor: ', i.author,'\nYear of publishment: ', i.yrOfPublishment, '\nNumber of pages:', i.nbrOfPages,'\nOwner: ', i.owner)
+    print('*'*15,'\nTitle: ',i.title, '\nAuthor: ', i.author,'\nYear of publishment: ', i.yrOfPublishment, '\nNumber of pages:', i.nbrOfPages,'\nOwner: ', i.owner_name)
