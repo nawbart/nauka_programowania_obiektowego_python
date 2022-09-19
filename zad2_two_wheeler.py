2fbf9f5a216acafc277f1dad945ca5365dadc05f
Bartosz Nawrocki <nawrockibartoszwro@gmail.com>
2022-09-19 18:36:11 +0200

    Podstawy dziedziczenia i abstrakcji w klasach

--- zad2_two_wheeler.py
+++ zad2_two_wheeler.py
@@ -1,8 +1,8 @@
 from abc import ABC, abstractmethod
 
-class two_wheeler(ABC):
+class TwoWheeler(ABC):
 
-    def __init__(self, color, brand, gearbox):
+    def __init__(self, color: str, brand: str, gearbox: str):
         self.color = color
         self.brand = brand
         self.gearbox = gearbox
@@ -10,28 +10,53 @@ class two_wheeler(ABC):
     def mileage(self):      #przebieg
         return self.mileage()
 
-    def go(self):       #metoda jedz, powodujaca zwiekszanie sie przebiegu
+    def go(self):       #metoda jedz, powodujaca zwiekszanie przebiegu o 1
         self.mileage += 1
 
     @abstractmethod
     def giveData(self):         #abstrakcyjna metoda podaj dane
         pass
 
-    class scooter(two_wheeler):
+class Scooter(TwoWheeler):
 
-        def __init__(self, color, brand, gearbox, trunk):     #trunk - bagaznik
-            super().__init__(color, brand, gearbox)
-            self.trunk = trunk
+    def __init__(self, color, brand, gearbox, trunk: bool):     #trunk - bagaznik
+        super().__init__(color, brand, gearbox)
+        self.trunk = trunk
 
-        def takePerson(self):
-            print("Here is", self.type, "i can ride with two people." )
+    def takePerson(self):
+        print('Here is scooter, i can ride with two people.')
+
+    def giveData(self):
+        print(self.brand, self.color, self.gearbox, self.trunk)
+        pass
+
+    def set_color(self, color):
+        self.color = color
+
+
+class Motocross(TwoWheeler):
+
+    def __init__(self, color, brand, gearbox, approval: bool):      #approval - homologacja
+        super().__init__(color, brand, gearbox)
+        self.approval = approval
+    def jump(self):
+        print('Here is cross i can jump over huge things')
+
+    def giveData(self):
+        print(self.brand, self.color, self.gearbox, self.approval)
+        pass
+
+def main():
+    skuter = Scooter('red', 'aprilia', 'automatic', True)
+    kros = Motocross('orange', 'KTM', 'manual', False)
+    skuter.set_color('green') # ten sposob jest lepszy do zmiany koloru niż ten ponizej
+    skuter.color = 'pink' # mozna ale sie nie powinno
+    skuter.giveData()
+    kros.giveData()
+    pass
+
+if __name__ == '__main__':
+    main()
 
 
 
-    class motocross(two_wheeler):
-
-        def __init__(self, color, brand, gearbox, approval):      #approval - homologacja
-            super().__init__(color, brand, gearbox)
-            self.approval = approval
-        def jump(self):
-            print("Here is", self.type, 'i can jump over huge things')
