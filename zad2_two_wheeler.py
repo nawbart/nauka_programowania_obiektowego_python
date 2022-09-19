e53cb435bc81c3b37edb2830db1486194ee32ac6
bartek <nawrockibartoszwro@gmail.com>
2022-09-19 15:44:27 +0200

    Dodanie konstruktorów do klas dziedziczących.

--- zad2_two_wheeler.py
+++ zad2_two_wheeler.py
@@ -2,8 +2,7 @@ from abc import ABC, abstractmethod
 
 class two_wheeler(ABC):
 
-    def __init__(self, type, color, brand, gearbox):
-        self.type = type
+    def __init__(self, color, brand, gearbox):
         self.color = color
         self.brand = brand
         self.gearbox = gearbox
@@ -20,6 +19,10 @@ class two_wheeler(ABC):
 
     class scooter(two_wheeler):
 
+        def __init__(self, color, brand, gearbox, trunk):     #trunk - bagaznik
+            super().__init__(color, brand, gearbox)
+            self.trunk = trunk
+
         def takePerson(self):
             print("Here is", self.type, "i can ride with two people." )
 
@@ -27,5 +30,8 @@ class two_wheeler(ABC):
 
     class motocross(two_wheeler):
 
+        def __init__(self, color, brand, gearbox, approval):      #approval - homologacja
+            super().__init__(color, brand, gearbox)
+            self.approval = approval
         def jump(self):
             print("Here is", self.type, 'i can jump over huge things')
